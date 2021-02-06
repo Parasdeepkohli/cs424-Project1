37405199bc1ebab42eed0015c0af6824ca50f38b
Parasdeep <56748097+Parasdeepkohli@users.noreply.github.com>
2021-02-07 00:00:27 +0530

    Stacked bar plot for percent energy per source added

--- ShinyApp.R
+++ ShinyApp.R
@@ -1,50 +1,29 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-source("Pre-processing.R")
-
-# Define UI for application that draws a histogram
+library(ggplot2)
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+    # *Input() functions
+    # *Output() functions
+    
+    plotOutput("hist")
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+    
+    output$hist <- renderPlot({ 
+        
+        no_total <- subset(mod_data_4, mod_data_4$ENERGY.SOURCE != "Total")
+        no_total$ENERGY.SOURCE <- as.factor(no_total$ENERGY.SOURCE)
+        ggplot(no_total, aes(fill = ENERGY.SOURCE, y = GENERATION..Megawatthours., x = YEAR)) + 
+            geom_bar(position = "fill", stat = "identity", width = 0.5) +
+            ggtitle("Percentage of total energy produced per energy source") +
+            xlab("Year") +
+            ylab("Percentage (%)") +
+            scale_x_continuous(breaks = seq(1990, 2019, by = 1),1) +
+            scale_y_continuous(breaks = seq(0, 1, by = 0.1),1)
     })
+    
+    
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
