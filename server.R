0edb9b6a2d09c579ccb259a60b69214fb41c77fa
Parasdeep <56748097+Parasdeepkohli@users.noreply.github.com>
2021-02-10 14:37:10 +0530

    Improved axis labels and legend

--- server.R
+++ server.R
@@ -10,13 +10,17 @@ function(input, output) {
     output$stackedbar1 <- renderPlot({
         
         ggplot(no_total, aes(fill = ENERGY.SOURCE, y = GENERATION..Megawatthours., x = YEAR)) + 
-            geom_bar(position = "stack", stat = "identity", width = 0.5) +
-            ggtitle("Amount of energy produced per year, per energy source") +
-            #scale_x_continuous(breaks = seq(1990, 2019, by = 1),1) +
-            #scale_y_continuous(breaks = seq(0, 1, by = 0.1),1) +
-            theme(text=element_text(size = 20, family = 'serif'), plot.title = element_text(hjust = 0.5)) +
+            geom_bar(position = "stack", stat = "identity", width = 0.6) +
+            ggtitle("Amount of energy produced in the US") +
+            theme(
+                text=element_text(size = 20, family = 'serif'), 
+                plot.title = element_text(hjust = 0.5, face = 'bold'),
+                legend.background = element_rect(fill = "darkgrey"),
+                ) +
+            labs(fill = "Energy Source") +
             scale_x_continuous(name = "Year", breaks = seq(1990, 2019)) +
-            scale_y_continuous(name = "Energy share (%)", breaks = seq(0,1, 0.1))
+            scale_y_continuous(name = "Energy produced in Millions (MWh)", breaks = seq(0, 1.5e10, by = 3e9), labels = c(seq(0, 15, by = 3)))
+            
         
     })
 
@@ -24,13 +28,18 @@ function(input, output) {
         
         
         ggplot(no_total, aes(fill = ENERGY.SOURCE, y = GENERATION..Megawatthours., x = YEAR)) + 
-            geom_bar(position = "fill", stat = "identity", width = 0.5) +
-            ggtitle("Percentage of total energy produced per year, per energy source") +
+            geom_bar(position = "fill", stat = "identity", width = 0.6) +
+            ggtitle("Percentage of total energy produced in the US") +
             #scale_x_continuous(breaks = seq(1990, 2019, by = 1),1) +
             #scale_y_continuous(breaks = seq(0, 1, by = 0.1),1) +
-            theme(text=element_text(size = 20, family = 'serif'), plot.title = element_text(hjust = 0.5)) +
+            theme(
+                text=element_text(size = 20, family = 'serif'), 
+                plot.title = element_text(hjust = 0.5, face = 'bold'),
+                legend.background = element_rect(fill = "darkgrey")
+                ) +
+            labs(fill = "Energy Source") +
             scale_x_continuous(name = "Year", breaks = seq(1990, 2019)) +
-            scale_y_continuous(name = "Energy share (%)", breaks = seq(0,1, 0.1))
+            scale_y_continuous(name = "Energy produced", labels = scales::percent)
     })
     
     
