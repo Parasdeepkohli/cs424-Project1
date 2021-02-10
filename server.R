748786c1f8ed89b6487d471ee4364b4152e985df
Parasdeep <56748097+Parasdeepkohli@users.noreply.github.com>
2021-02-10 18:54:30 +0530

    Added global nav bar and local side panels

--- server.R
+++ server.R
@@ -5,42 +5,47 @@ source("Pre-processing.R")
 no_total <- subset(mod_data, mod_data$ENERGY.SOURCE != "Total")
 no_total$ENERGY.SOURCE <- as.factor(no_total$ENERGY.SOURCE)
 
-function(input, output) {
+function(input, output, session) {
+
+
     
-    output$stackedbar1 <- renderPlot({
+    output$chart1 <- renderPlot({
         
-        ggplot(no_total, aes(fill = ENERGY.SOURCE, y = GENERATION..Megawatthours., x = YEAR)) + 
-            geom_bar(position = "stack", stat = "identity", width = 0.6) +
-            ggtitle("Amount of energy produced in the US") +
-            theme(
-                text=element_text(size = 20, family = 'serif'), 
-                plot.title = element_text(hjust = 0.5, face = 'bold'),
-                legend.background = element_rect(fill = "darkgrey"),
-                ) +
-            labs(fill = "Energy Source") +
-            scale_x_continuous(name = "Year", breaks = seq(1990, 2019)) +
-            scale_y_continuous(name = "Energy produced in Millions (MWh)", breaks = seq(0, 1.5e10, by = 3e9), labels = c(seq(0, 15, by = 3)))
+        if (input$visualization == "Bar plot"){
             
-        
+            ggplot(no_total, aes(fill = ENERGY.SOURCE, y = GENERATION..Megawatthours., x = YEAR)) + 
+                geom_bar(position = "stack", stat = "identity", width = 0.6) +
+                ggtitle("Amount of energy produced in the US") +
+                theme(
+                    text=element_text(size = 18, family = 'sans'), 
+                    plot.title = element_text(hjust = 0.5, face = 'bold'),
+                    legend.background = element_rect(fill = "darkgrey")
+                    ) +
+                labs(fill = "Energy Source") +
+                scale_x_continuous(name = "Year", breaks = seq(1990, 2019)) +
+                scale_y_continuous(name = "Energy produced in Millions (MWh)", breaks = seq(0, 1.6e10, by = 4e9), labels = c(seq(0, 16, by = 4)))
+        }
     })
 
-    output$stackedbar2 <- renderPlot({ 
+    output$chart2 <- renderPlot({
         
-        
-        ggplot(no_total, aes(fill = ENERGY.SOURCE, y = GENERATION..Megawatthours., x = YEAR)) + 
-            geom_bar(position = "fill", stat = "identity", width = 0.6) +
-            ggtitle("Percentage of total energy produced in the US") +
-            #scale_x_continuous(breaks = seq(1990, 2019, by = 1),1) +
-            #scale_y_continuous(breaks = seq(0, 1, by = 0.1),1) +
-            theme(
-                text=element_text(size = 20, family = 'serif'), 
-                plot.title = element_text(hjust = 0.5, face = 'bold'),
-                legend.background = element_rect(fill = "darkgrey")
-                ) +
-            labs(fill = "Energy Source") +
-            scale_x_continuous(name = "Year", breaks = seq(1990, 2019)) +
-            scale_y_continuous(name = "Energy produced", labels = scales::percent)
+        if (input$visualization == "Bar plot"){
+            
+            ggplot(no_total, aes(fill = ENERGY.SOURCE, y = GENERATION..Megawatthours., x = YEAR)) + 
+                geom_bar(position = "fill", stat = "identity", width = 0.6) +
+                ggtitle("Percentage of total energy produced in the US") +
+                #scale_x_continuous(breaks = seq(1990, 2019, by = 1),1) +
+                #scale_y_continuous(breaks = seq(0, 1, by = 0.1),1) +
+                theme(
+                    text=element_text(size = 20, family = 'serif'), 
+                    plot.title = element_text(hjust = 0.5, face = 'bold'),
+                    legend.background = element_rect(fill = "darkgrey")
+                    ) +
+                labs(fill = "Energy Source") +
+                scale_x_continuous(name = "Year", breaks = seq(1990, 2019)) +
+                scale_y_continuous(name = "Energy produced", labels = scales::percent)
+        }
     })
     
-    
+   
 }
--- ui.R
+++ ui.R
@@ -1,9 +1,36 @@
 library(shiny)
 
-fluidPage(
+navbarPage(
   # *Input() functions
   # *Output() functions
+  title = "Welcome",
+  id = "nav",
+  position = "static-top",
+  collapsible = TRUE,
+  selected = "Basic Visualizations",
+  tabPanel(title = "About"),
   
-  plotOutput("stackedbar1"),
-  plotOutput("stackedbar2")
+  tabPanel("Basic Visualizations",
+    sidebarLayout(       
+      sidebarPanel(
+        selectInput(inputId = "visualization", label = "Visualization", choices = c("Bar plot", "Line chart", "Table")),
+        width = 2,
+        conditionalPanel(
+          condition = "input.visualization == 'Line chart'",
+          checkboxGroupInput(
+            inputId = "lineSources", 
+            label = "Pick the sources", 
+            choices = c( "Coal", "Geo Thermal", "Hydro", "Natural Gas", "Nuclear", "Petrol", "Solar","Wind", "Wood")
+            )
+          )
+        ),
+      mainPanel(
+        width = 10,
+        plotOutput("chart1"),
+        plotOutput("chart2"),
+      )
+    )
+  )
+  
+
 )
