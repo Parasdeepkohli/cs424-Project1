f42d02858539859dd8de3e7077b66ca1038df27f
Parasdeep <56748097+Parasdeepkohli@users.noreply.github.com>
2021-02-12 18:24:03 +0530

    Dynamic units for line plots

---
+++ Add-units.R
@@ -0,0 +1,8 @@
+addUnits <- function(n) {
+  labels <- ifelse(n < 1000, n,  # less than thousands
+                   ifelse(n < 1e6, paste0(round(n/1e3), 'k'),  # in thousands
+                          ifelse(n < 1e9, paste0(round(n/1e6), ' Million'),  # in millions
+                                 ifelse(n < 1e12, paste0(round(n/1e9), ' Billion'), # in billions
+                                        ))))
+  return(labels)
+}
