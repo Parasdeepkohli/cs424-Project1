66e05d9c6911ac3c5f28ce7bc4585503567d96d0
Parasdeep <56748097+Parasdeepkohli@users.noreply.github.com>
2021-02-12 22:01:04 +0530

    Bug fixes for 0 and near-0 values

--- Add-units.R
+++ Add-units.R
@@ -1,8 +1,7 @@
 addUnits <- function(n) {
-  print(n)
-  labels <- ifelse(n < 100, paste0(round(n, digits = 2), '%'),  # percentages
-                          ifelse(n < 1e9, paste0(round(n/1e6), ' Million'),  # in millions
-                                 ifelse(n < 1e12, paste0(round(n/1e6)/1000, ' Billion'), '' # in Billions
+  labels <- ifelse((n < 100) & (n > 0), paste0(round(n, digits = 2), '%'),  # percentages
+                          ifelse((n < 1e9) & (n > 0), paste0(round(n/1e6), ' Million'),  # in millions
+                                 ifelse((n < 1e12) & (n > 0), paste0(round(n/1e6)/1000, ' Billion'), '' # in Billions
                                         )))
   return(labels)
 }
