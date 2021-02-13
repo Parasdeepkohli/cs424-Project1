71018172b160ff914cebbee715f1279c6f8dff0d
Parasdeep <56748097+Parasdeepkohli@users.noreply.github.com>
2021-02-14 04:27:56 +0530

    bug fixing for values = 100

--- Add-units.R
+++ Add-units.R
@@ -1,5 +1,5 @@
 addUnits <- function(n) {
-  labels <- ifelse((n < 100) & (n > 0), paste0(round(n, digits = 2), '%'),  # percentages
+  labels <- ifelse((n <= 100) & (n > 0), paste0(round(n, digits = 2), '%'),  # percentages
                    ifelse((n<1e6) & (n > 0), paste0(round(n/1e3), ' thousand'), # in thousands
                           ifelse((n < 1e7) & (n > 0), paste0(round(n/1e3)/1000, ' Million'), # To handle decimal millions
                             ifelse((n < 1e9) & (n > 0), paste0(round(n/1e6), ' Million'),  # in millions
